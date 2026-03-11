c7fe3e6228a2bd8e609405d33eb877ae50cfd4b5
MajidZanjani <majidmmz1972@gmail.com>
2026-03-11 11:38:57 +0400

    feat: implement argparser

--- data-processing-cli/src/utils/aargParser.js
+++ data-processing-cli/src/utils/aargParser.js
@@ -1,1 +1,14 @@
-// parse command line arguments
+export function parseArgs(args) {
+  const result = {};
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].startsWith("--")) {
+      const key = args[i].slice(2);
+      const value = args[i + 1];
+      result[key] = value;
+      i++;
+    }
+  }
+
+  return result;
+}
